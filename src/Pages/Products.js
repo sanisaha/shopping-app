adb8ee5f9aaebd7688f3fd2fbe1beb4e7275f38d
sanisaha <ratulcvasu@gmail.com>
2023-09-14 14:49:06 +0300

    excess code deleted

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -8,12 +8,6 @@ const {data:products} = await axios.get(api);
 console.log(products);
 
 const Products = () => {
-    // const [products, setProducts] = useState([]);
-    
-    // useEffect(()=>{
-    //     fetch(api).then(res=>res.json()).then(data=>setProducts(data))
-    // },[])
-    
     
     return (
         <div className='container'>
